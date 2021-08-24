99e906e3761804dc9a1d4a367db245642c97340d
jgumerove <josh.gumerove@gmail.com>
2021-08-24 13:59:11 -0400

    commit tree 1f113e9ad93ac585feb96fa75d50aae95d9521c4

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+function saturdayFun (activity="roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function (activity="go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective (flair="*") {
+    return function(string="special") {
+        return (`You are ${flair}${string}${flair}!`)
+    }
+}
+
+const Calculator = {
+    add: function() {
+        return 1 + 3
+    },
+
+    subtract: function() {
+        return 1 - 3
+    },
+
+    multiply: function() {
+        return 1 * 3
+    },
+
+    divide: function() {
+        return 10 / 5
+    }
+}
+
+function actionApplyer(integer, array){
+
+    if(array === []){
+        return integer
+    }else{
+        for(let i = 0; i < array.length; i++){
+            integer = array[i](integer);
+        }
+        return integer
+    }
+
+}
